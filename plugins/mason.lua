1aa12262ab550ca52b9fe2891f4d9063d05b8c40
Lilanga Gamage <lilanga@live.com>
2023-09-19 19:54:55 +1000

    setup_handlers not needed with mason-null-ls 2.0.0 release

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -25,11 +25,6 @@ return {
       local mason_null_ls = require "mason-null-ls"
       local null_ls = require "null-ls"
       mason_null_ls.setup(opts)
-      mason_null_ls.setup_handlers {
-        prettierd = function()
-          null_ls.register(null_ls.builtins.formatting.prettierd.with { extra_filetypes = { "json", "markdown" } })
-        end,
-      }
     end,
   },
 }
