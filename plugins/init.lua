3da8bbc9f53d8afcd369c182eceeb9bedac82a6b
Lilanga Gamage <lilanga@live.com>
2023-09-19 22:06:08 +1000

    fix treesitter issues

--- plugins/init.lua
+++ plugins/init.lua
@@ -80,7 +80,41 @@ return {
 
   --- Functionality ---
   ---------------------
+  {
+    "folke/twilight.nvim",
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  },
+  -- Org mode
+  {
+    "nvim-orgmode/orgmode",
+    dependencies = {
+      { "nvim-treesitter/nvim-treesitter", lazy = true },
+    },
+    event = "VeryLazy",
+    config = function()
+      -- Load treesitter grammar for org
+      require("orgmode").setup_ts_grammar()
 
+      -- Setup treesitter
+      require("nvim-treesitter.configs").setup {
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = { "org" },
+        },
+        ensure_installed = { "org" },
+      }
+
+      -- Setup orgmode
+      require("orgmode").setup {
+        org_agenda_files = "~/orgfiles/**/*",
+        org_default_notes_file = "~/orgfiles/refile.org",
+      }
+    end,
+  },
   -- tagbar, needs ctags to work.
   {
     "preservim/tagbar",
--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
+    ignore_install = { "help" },
     ensure_installed = {
       "c",
       "lua",
